eb87f8ec50e5c9eac0e72361777b63e55d7345bf
infinyte7 <manikant.gautam.712@gmail.com>
2020-11-28 19:22:34 +0800

    Version 1.3.7

--- android/app/src/main/java/io/infinyte7/ankiimageocclusion/MainActivity.java
+++ android/app/src/main/java/io/infinyte7/ankiimageocclusion/MainActivity.java
@@ -19,18 +19,25 @@
 
 package io.infinyte7.ankiimageocclusion;
 
-import android.content.Context;
-import android.content.pm.PackageInfo;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.widget.Toast;
 
 import org.apache.cordova.*;
 
+import com.ichi2.anki.api.AddContentApi;
+
 public class MainActivity extends CordovaActivity
 {
+    private static final int PERMISSION_REQUEST_CODE = 200;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -43,26 +50,12 @@ public class MainActivity extends CordovaActivity
             moveTaskToBack(true);
         }
 
-        boolean permissionAllowed = true;
-
-        Context context = this.cordovaInterface.getActivity().getApplicationContext();
-
-        final PackageManager pm = context.getPackageManager();
-        try {
-            PackageInfo packageInfo = pm.getPackageInfo(context.getPackageName(), PackageManager.GET_PERMISSIONS);
-            String[] requestedPermissions = packageInfo.requestedPermissions;
-            if ( requestedPermissions != null ) {
-                for (int i = 0; i < requestedPermissions.length; i++) {
-                    if ((packageInfo.requestedPermissionsFlags[i] & PackageInfo.REQUESTED_PERMISSION_GRANTED) == 0) {
-                        Toast.makeText(context, "Allow storage and additional permissions from app settings", Toast.LENGTH_LONG).show();
-                        permissionAllowed = false;
-                        break;
-                    }
-                }
-            }
-        }
-        catch ( PackageManager.NameNotFoundException e ) {
-            Toast.makeText(context, "AnkiDroid not installed.", Toast.LENGTH_LONG).show();
+        if (checkPermission()) {
+            // Set by <content src="index.html" /> in config.xml
+            loadUrl(launchUrl);
+        } else {
+            requestPermission();
+            loadUrl("file:///android_asset/www/permissions.html");
         }
 
         super.appView.getView().setHorizontalScrollBarEnabled(false);
@@ -74,11 +67,61 @@ public class MainActivity extends CordovaActivity
         settings.setDefaultZoom(WebSettings.ZoomDensity.MEDIUM);
         settings.setSupportZoom(true);
 
-        if (permissionAllowed) {
-            // Set by <content src="index.html" /> in config.xml
-            loadUrl(launchUrl);
-        } else {
-            loadUrl("file:///android_asset/www/permissions.html");
+    }
+
+    /* https://stackoverflow.com/questions/42275906/how-to-ask-runtime-permissions-for-camera-in-android-runtime-storage-permissio */
+    private boolean checkPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, AddContentApi.READ_WRITE_PERMISSION) != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            return false;
         }
+        return true;
+    }
+
+    private void requestPermission() {
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, AddContentApi.READ_WRITE_PERMISSION},
+                PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
+                    loadUrl(launchUrl);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED &&
+                                ContextCompat.checkSelfPermission(this, AddContentApi.READ_WRITE_PERMISSION)
+                                        != PackageManager.PERMISSION_GRANTED) {
+                            showMessageOKCancel("You need to allow Storage and AnkiDroid read write permissions",
+                                    new DialogInterface.OnClickListener() {
+                                        @Override
+                                        public void onClick(DialogInterface dialog, int which) {
+                                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                                requestPermission();
+                                            }
+                                        }
+                                    });
+                        }
+                    }
+                }
+                break;
+        }
+    }
+
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(MainActivity.this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
     }
 }
